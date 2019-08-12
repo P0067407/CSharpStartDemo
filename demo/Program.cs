24c0eafcca615743e6306a1ed161a3f6655b6cdd
P0067407 <qingpeng.wang@pactera.com>
2019-08-12 10:01:18 +0800

    代码提交

---
+++ demo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string size = "444";
+            Console.WriteLine(size);
+            Console.WriteLine($"{444}");
+            Console.WriteLine("abcdefgh".Substring(2));
+            Console.WriteLine("abcdefgh".Substring(2, 3));
+            Console.WriteLine("abcdefghabcd".Replace("abcd","xxxx"));
+            int random = new Random().Next(1000);
+            Console.WriteLine(random);
+        }
+         
+    }
+}
